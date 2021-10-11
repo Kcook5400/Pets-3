8b9e85bebec0a62fb5a68833c87ba9ee4d92fcb8
Colby Boell <68253967+cmboell@users.noreply.github.com>
2021-10-11 13:29:30 -0500

    fixed createNewListDetails
    fixed error

--- src/main/java/controller/CreateNewListServlet.java
+++ src/main/java/controller/CreateNewListServlet.java
@@ -11,22 +11,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import model.Pet;
 import model.AppointmentList;
 import model.Customer;
-
+import model.Pet;
 
 /**
- * Servlet implementation class CreateNewListServlet
+ * Servlet implementation class createNewListServlet2
  */
-@WebServlet("/createNewListServlet")
-public class CreateNewListServlet extends HttpServlet {
+@WebServlet("/createNewListServlet2")
+public class createNewListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public CreateNewListServlet() {
+    public createNewListServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -35,45 +34,8 @@ public class CreateNewListServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		AppointmentListHelper lih = new AppointmentListHelper();
-		String listName = request.getParameter("listName");
-		System.out.println("List Name: " + listName);
-		
-		String month = request.getParameter("month");
-		String day = request.getParameter("day");
-		String year = request.getParameter("year");
-		String CustomerName = request.getParameter("CustomerName");
-		LocalDate ld;
-		try {
-			ld = LocalDate.of(Integer.parseInt(year),Integer.parseInt(month), Integer.parseInt(day));
-			}
-		catch (NumberFormatException ex) {
-			ld = LocalDate.now();
-			}
-		String [] selectedItems= request.getParameterValues("allPetsToAdd");
-		List<AppointmentList> selectedItemsInList = new ArrayList <AppointmentList> ();
-		if(selectedItems != null && selectedItems.length > 0) {
-			for (int i =0; i<selectedItems.length; i++) {
-				System.out.println(selectedItems[i]);
-				AppointmentList c = lih.searchForListDetailsById(Integer.parseInt(selectedItems[i]));
-				selectedItemsInList.add(c);
-			}
-			
-		}
-		Customer customer = new Customer(CustomerName);
-		AppointmentList sld = new AppointmentList(listName, customer, ld);
-		sld.setPetsList(selectedItemsInList);
-		AppointmentListHelper slh = new AppointmentListHelper();
-		slh.insertNewListDetails(sld);
-		
-		System.out.println("Success!");
-		System.out.println(sld.toString());
-		
-		getServletContext().getRequestDispatcher("/viewAllListsServlet").forward(request, response);
-		
-		
-		
-		
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
@@ -81,7 +43,37 @@ public class CreateNewListServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		PetHelper lih = new PetHelper();
+		String appointmentName = request.getParameter("appointmentName");
+		System.out.println("List Name: "+ appointmentName);
+		String month = request.getParameter("month");
+		String day = request.getParameter("day");
+		String year = request.getParameter("year");
+		String customerName = request.getParameter("customerName");
+		LocalDate ld;
+		try {
+		ld = LocalDate.of(Integer.parseInt(year), Integer.parseInt(month), Integer.parseInt(day));
+		}catch(NumberFormatException ex) {
+		ld = LocalDate.now();
+		}
+		String[] selectedItems = request.getParameterValues("allItemsToAdd");
+		List<Pet> selectedItemsInList = new ArrayList<Pet>();
+		if (selectedItems != null && selectedItems.length > 0) 
+		{
+		for(int i = 0; i<selectedItems.length; i++) {
+		System.out.println(selectedItems[i]);
+		Pet c = lih.searchForPetById(Integer.parseInt(selectedItems[i]));
+		selectedItemsInList.add(c);
+		}
+		}
+		Customer customer = new Customer(customerName);
+		AppointmentList sld = new AppointmentList(appointmentName, customer, ld);
+		sld.setPetsList(selectedItemsInList);
+		AppointmentListHelper slh = new AppointmentListHelper();
+		slh.insertNewListDetails(sld);
+		System.out.println("Success!");
+		System.out.println(sld.toString());
+		getServletContext().getRequestDispatcher("/viewAllListsServlet").forward(request, response);
 	}
 
 }
